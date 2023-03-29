09002ed0d0c0180a3b0d7c9cb3e01f4b60110982
Tony <65500763+bit-burger@users.noreply.github.com>
2023-03-30 00:02:58 +0200

    Add change command to manually change mo credits

---
+++ application/slashCommands/change.js
@@ -0,0 +1,33 @@
+const dao = require("../../database/mo_credit_dao")
+
+const { SlashCommandBuilder, userMention } = require("discord.js");
+
+const moschopsGif = "https://media.tenor.com/1j4ZfgiFYY0AAAAC/moschops.gif";
+
+module.exports = {
+    // Setting command information and options
+    data: new SlashCommandBuilder().setName("change").setDescription("change credits of a single user")
+        .addUserOption((option) =>
+            option.setName("who").setDescription("who to query results from").setRequired(true)
+        )
+        .addIntegerOption((option) =>
+            option.setName("how_much")
+                .setDescription("how many credits to add (if positive) or subtract (if negative)")
+                .setRequired(true)),
+
+    // Handling command autocomplete
+    async autocomplete(interaction) { },
+
+    // Handling command reponse
+    async execute(interaction) {
+        const user = interaction.options.getUser("who")
+        let credits = await dao.getCredits(interaction.client, interaction.user.id)
+        credits += interaction.options.getInteger("how_much")
+        await dao.setCredits(interaction.client, user.id, credits)
+        interaction.reply({
+            content: "The credits of " + userMention(user.id) + " have been fixed! :muscle::muscle::muscle::muscle:\n" +
+                `They now have ${credits} mo credits:flushed:`,
+        })
+        interaction.channel.send({ content: "https://media.tenor.com/1j4ZfgiFYY0AAAAd/moschops.gif" })
+    },
+};
